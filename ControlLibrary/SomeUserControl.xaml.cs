9cb958d17be442cbf1bbe11c7b6dee1eccd1df28
Joshua D Miller <joshdan65@gmail.com>
2017-08-25 10:30:54 -0500

    Initial commit

---
+++ ControlLibrary/SomeUserControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace ControlLibrary
+{
+    /// <summary>
+    /// Interaction logic for SomeUserControl.xaml
+    /// </summary>
+    public partial class SomeUserControl : UserControl
+    {
+        public SomeUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
